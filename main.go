ed40e6b5d24c843878865ff31b3f0b150ca0fefe
Jules Michael <julesmichaelgiovanni@gmail.com>
2020-09-09 23:51:21 +0400

    init commit cloud function

--- main.go
+++ main.go
@@ -1,31 +1,11 @@
-package main
+package helloworld
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"os"
 )
 
-func main() {
-	log.Print("starting server...")
-	http.HandleFunc("/", handler)
-
-	// Determine port for HTTP service.
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("defaulting to port %s", port)
-	}
-
-	// Start HTTP server.
-	log.Printf("listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
-}
-
-// handler func for '/'
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World! More people!")
+// HelloHTTP is an HTTP cloud function
+func HelloHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World! I'm a Cloud Function!")
 }
